f3fb28a995c3704b6fcda346a569e36c5ef4fc4a
ChristelleDS <christelle.dasilva75@gmail.com>
2022-11-09 09:59:44 +0100

    init

---
+++ appEvents/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+
+class Client(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=20)
+    company_name = models.CharField(max_length=250)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    # sales_contact =
